915864466cb3c3435a143eef522c2752c38a3c82
Roudy Antenor <roudyantenor@gmail.com>
2019-02-26 15:02:32 -0500

    addition of button that once pressed adds selection as list items to the given dom - while maintaining the highlighted portions of text

--- main.js
+++ main.js
@@ -1,46 +1,56 @@
 'use strict';
 
 let outlineElement = document.getElementsByClassName('outline')[0]
-let chosenSelection;
+let myButton = document.getElementsByClassName('create')[0];
+let coolList = document.getElementsByClassName('cool-list')[0];
+let chosenSelection; //the main event!
 let chosenStringified; //variable holding our chosen text to string
+
+let soComplete;
 
 
 document.onselectionchange = () => {
   console.log("A selection is being made...")
   chosenSelection = document.getSelection();
-  chosenStringified = chosenSelection.toString()
-  outlineElement.textContent = chosenStringified; // add to DOM all above
+  chosenStringified = chosenSelection.toString()//can erase not used
+   
 
 
   if (document.getSelection().type === "Range") { //if text were selected
-    console.log("Something has been selected");
+    //console.log("Something has been selected");
     //grab the string text of the paragraph - assigne to variable
-    let entiretyOfEle = chosenSelection.baseNode;
-    console.log(entiretyOfEle);  // lets see the text
-    // console.log(getSelection().getRangeAt(0))
+    //let entiretyOfEle = chosenSelection.baseNode;
+    //console.log(entiretyOfEle);  // lets see the text
 
 
 
+    let newNode, range;
     /** Create a SPAN around the selected RANGE on mouse up */
     document.onmouseup = () => {
-      let newNode = document.createElement('span');
+      newNode = document.createElement('span');
       newNode.className = "red-highlight"
-      let range = getSelection().getRangeAt(0);
+      range = getSelection().getRangeAt(0);
       range.surroundContents(newNode);
+      //outlineElement.textContent = chosenStringified; // add to DOM all above
+      soComplete = chosenStringified
+      console.log(soComplete);
     }
 
-
-
-
-    //WAIT 
-    // let beganSelection = stringTextOfParagraph.indexOf(chosenSelection);
-    // console.log(beganSelection);
-    //stringTextOfParagraph.substring(beganSelection,  ----make this a function value)
-    
-    //
-
   }
-  
 }
+
+
+
+
+
+/**Why i could not just do this below Is BEYOND ME!! */
+// myButton.addEventListener('mousedown', addToDom());
+/**BUtton that will add the selection as a Line Item to DOM */
+myButton.addEventListener('mousedown', (e) => {
+  let listItem = document.createElement('li');
+  listItem.textContent = soComplete;
+  coolList.appendChild(listItem);
+})
+
 
 
